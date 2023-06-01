b1722b6ae1794d643119abd178fa0a4751732375
Pedro-Henrique-Costa <pedrohcosta0209@gmail.com>
2023-06-01 20:59:54 -0300

    feat: create json server

--- src/configs/api/index.ts
+++ src/configs/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //pode usar outra api basta abrir a porta e mudar a URL aqui
 export const api = axios.create({
-    baseURL: 'http://api.jsonserver.io/',
+    baseURL: 'https://api.jsonserver.io/',
     headers: {
         "X-Jsio-Token" : "93f7cb4e343d536ee78ed32e71342735"
     }
